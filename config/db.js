9cd736bd9e06cd3a423c3faae3bd31d9dd781d32
Calin Florescu <calin.florescu@fanduel.com>
2022-01-30 15:15:34 +0200

    feat: refactor mongodb connection logic

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const logger = require('../src/utils/logger')
+
+module.exports = () => {
+    mongoose.connect(process.env.MONGO_URI, 
+        { 
+            useNewUrlParser: true, 
+            useUnifiedTopology: true 
+        });
+    
+    const mongoDbConnection = mongoose.connection
+    
+    mongoDbConnection.on('error', console.error.bind(console, "connection error: "))
+    mongoDbConnection.on('open', () => {
+        logger.info('Connected to MongoDB cluster')
+    })
+}
